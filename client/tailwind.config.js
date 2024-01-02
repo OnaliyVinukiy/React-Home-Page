ed0d1048ba04874b1b6a5f8388ad520ca0fb99a5
avish <avishmadushanka21@gmail.com>
2024-01-02 12:27:57 +0530

    slider

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -8,4 +9,4 @@ export default {
     extend: {},
   },
   plugins: [],
-}
+});
